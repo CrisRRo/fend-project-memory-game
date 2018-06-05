2ba008443ac210d16cd19c6ade77e61cef9eb142
CrisRRo <rcristina76@yahoo.com>
2018-06-05 14:29:53 +0300

    Show the card when is clicked

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
+ 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -25,7 +25,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +35,19 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ const gameDeck = document.querySelector(".deck");
+ let openedCard = null;
+
+ // Add an event listener to the cards
+ gameDeck.addEventListener('click', function revealCard(event) {
+	// Check to see if the card is clicked, not the border of the deck
+	if (event.target.nodeName === 'LI') {
+		showCard(event);
+	}
+}, false);
+
+// show the card when it is clicked (set class to open show)
+function showCard(event){
+	event.target.setAttribute("class", "card open show");
+}
