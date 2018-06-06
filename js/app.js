dcd0df1df6bacc4fa00947b679a297d69242e500
CrisRRo <rcristina76@yahoo.com>
2018-06-06 12:18:08 +0300

    Set the function that manipulates cards when clicked

--- js/app.js
+++ js/app.js
@@ -25,6 +25,11 @@ function shuffle(array) {
     return array;
 }
 
+/* Used like so - TO BE DELETED
+var arr = [2, 11, 37, 42];
+arr = shuffle(arr);
+console.log(arr);  */
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,18 +41,86 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+ // Get the container of the cards
  const gameDeck = document.querySelector(".deck");
- let openedCard = null;
+
+ // For every 2 clicks, store in these variable the element and the value of the first clicked card
+ let firstClickedElement = null;
+ let firstCard = null;
+
+ // Keep track of number of matched cards
+ let cellNo = 0;
 
  // Add an event listener to the cards
- gameDeck.addEventListener('click', function revealCard(event) {
-	// Check to see if the card is clicked, not the border of the deck
-	if (event.target.nodeName === 'LI') {
-		showCard(event);
-	}
+ gameDeck.addEventListener('click', function(event){
+	 manipulateCard(event);
 }, false);
 
-// show the card when it is clicked (set class to open show)
+// Manipulate cards on click according to their status
+function manipulateCard(event) {
+	// Store this card attribute
+	let thisCard = event.target.getAttribute("class");
+
+	// Check to see if a card is clicked, not the border of the deck && if that card is not already with face up (his class is 'card match' or 'card open show' (otherwise nothing happens)
+	if ((event.target.nodeName === 'LI') && !thisCard.includes("match") && !thisCard.includes("open")){
+
+		// Check if there is any card already clicked
+		if (firstClickedElement === null){
+
+			// No card clicked yet => store its value (class) into firstCard variable
+			firstCard = event.target.lastElementChild.getAttribute("class");
+
+			// Show the card
+			showCard(event);
+
+			// Get the element of the first clicked card
+			firstClickedElement = document.querySelector(".clicked");
+
+		} else if (firstCard === event.target.lastElementChild.getAttribute("class")) {
+				// Show the second clicked card
+				showCard(event);
+
+							// TODO: set a DELAY
+				// Since 2nd card matches the first one => change cards status to "card match" (both cards remain with their face up)
+				changeCardsStatus(event, "card match");
+
+				// Increase number of matched cards
+				cellNo = cellNo + 2;
+
+				// Reinitialize to null (a new pair of clicks begins)
+				other2cards();
+
+			} else {
+				// Show the second clicked card
+				showCard(event);
+
+							// TODO: set a DELAY
+				// Set the 2 clicked cards attributes to wrong class
+				changeCardsStatus(event, "card open show wrong");
+
+							// TODO: set a DELAY
+				// Set the 2 clicked cards attributes to its defaults
+				changeCardsStatus(event, "card");
+
+				// Reinitialize to null (a new pair of clicks begins)
+				other2cards();
+			}
+	}
+}
+
+// Show the card when it is clicked (change its class to open show)
 function showCard(event){
-	event.target.setAttribute("class", "card open show");
+	event.target.setAttribute("class", "card open show clicked");
+}
+
+// Change status of the last 2 clicked cards
+function changeCardsStatus(event, cardStatus){
+	firstClickedElement.setAttribute("class", cardStatus);
+	event.target.setAttribute("class", cardStatus);
+}
+
+// Reinitialize to null (a new pair of clicks begins)
+function other2cards(){
+	firstClickedElement = null;
+	firstCard = null;
 }
